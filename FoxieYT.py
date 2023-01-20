54b2a90c6e43b23406eb61c754e6ba1a85e38dc0
DevFoxie <hmerniz.dev@gmail.com>
2023-01-20 10:28:10 +0100

    DevFoxie YT App

--- FoxieYT.py
+++ FoxieYT.py
@@ -1,20 +1,13 @@
 import tkinter as tk
 from tkinter import simpledialog
-from tkinter import ttk
 from pytube import YouTube
 import subprocess
 
 root = tk.Tk()
 root.withdraw()
-ttk.Style().theme_use('clam')
-root.geometry("1200x1200")
 
 youtube_url = simpledialog.askstring(title="DevFoxie YouTube :)",
-                                     prompt="Entrez l'URL de la vidéo YouTube à lire:",
-                                     parent=root,
-                                     initialvalue='',
-                                     minvalue=0,
-                                     maxvalue=100)
+                                     prompt="Entrez l'URL de la vidéo YouTube à lire:")
 
 yt = YouTube(youtube_url)
 stream = yt.streams.filter(file_extension='mp4').first()
